28bacf66b7364e9d0fa84d8abeccda4a3a43010a
Student <student@CS5600-f15-Ubuntu32.defaultdomain>
2015-09-17 14:38:20 -0400

    Adding a sample project

---
+++ C-projectSample/src/hw1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+/* The main function for this project */
+
+	int counter;
+	
+	for (counter = 1; counter <= 10; counter++)
+	{
+	
+		printf("\n%d",counter);
+		sleep(1);	
+	}
+return 0;
+}
